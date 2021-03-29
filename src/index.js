5113af7ba0add801d74977c58c58c38095e5dbde
waruwat <waruwat.work@gmail.com>
2021-03-29 17:08:16 +0700

    update workflow deploy to ecs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('lab9 Done!'))
 
 app.listen(port, "0.0.0.0", () => console.log(`Web app listening on port ${port}!`))
