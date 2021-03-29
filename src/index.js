c931291f16a6e0d90a97208a37c1038c09f18ae5
waruwat <waruwat.work@gmail.com>
2021-03-29 17:13:16 +0700

    update respon

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('lab9 Done!'))
+app.get('/', (req, res) => res.send('lab9 Done ! 61070214'))
 
 app.listen(port, "0.0.0.0", () => console.log(`Web app listening on port ${port}!`))
